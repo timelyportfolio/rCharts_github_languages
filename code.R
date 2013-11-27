b2a9d53288094634390bee803b7a27be973aedcb
timelyportfolio <kent.russell@timelyportfolio.com>
2013-11-27 15:19:33 -0600

    finally getting useable results

--- code.R
+++ code.R
@@ -8,9 +8,28 @@ languages <- c("python","javascript","r")
 ctx <- create.github.context("https://api.github.com/")
 
 results <- lapply(languages,function(lang) {
-  search.repositories(
+  result <- search.repositories(
     ctx,
     q=sprintf("language:%s",lang),
     "&sort=stars&order=desc"
-  )$content
+  )
 })
+
+names(results) <- languages
+
+lapply(
+  languages,
+  function(lang){
+    lapply(
+      results[[lang]]$content$items,
+      function(repo){
+          return(data.frame(
+            lang,
+            repo$name,
+            repo$stargazers_count,
+            repo$forks
+          ))
+      }
+    )
+  }
+)
