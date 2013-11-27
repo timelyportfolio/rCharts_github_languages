94e12bb38a4d445d2d25694d592e3188be80d00f
timelyportfolio <kent.russell@timelyportfolio.com>
2013-11-27 14:59:33 -0600

    use rgithub package

--- code.R
+++ code.R
@@ -1,2 +1,16 @@
+#require(devtools)
+#install_github("rgithub","cscheid")
+library(github)
+
 languages <- c("python","javascript","r")
 
+#get our ctx to use with github api
+ctx <- create.github.context("https://api.github.com/")
+
+results <- lapply(languages,function(lang) {
+  search.repositories(
+    ctx,
+    q=sprintf("language:%s",lang),
+    "&sort=stars&order=desc"
+  )$content
+})
