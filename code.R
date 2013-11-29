e0ce94679a19f5870b84b8e20bc64561f5bda2b6
timelyportfolio <kent.russell@timelyportfolio.com>
2013-11-29 15:26:00 -0600

    add some other lattice graphs even though eventually want all interactive rcharts

--- code.R
+++ code.R
@@ -2,7 +2,7 @@
 #install_github("rgithub","cscheid")
 library(github)
 
-languages <- c("python","javascript","ruby","r")
+languages <- c("python","java","javascript","ruby","r")
 
 #get our ctx to use with github api
 ctx <- create.github.context("https://api.github.com/")
@@ -11,7 +11,7 @@ results <- lapply(languages,function(lang) {
   result <- search.repositories(
     ctx,
     q=sprintf("language:%s",lang),
-    "&sort=stars&order=desc"
+    "&sort=stars&order=desc&per_page=100"
   )
 })
 
@@ -38,4 +38,33 @@ info.df <- do.call(rbind,lapply(
 
 require(lattice)
 #simple stupid graph to get a first look
-xyplot(log(repo.stargazers_count)~repo.name,groups=lang,data=info.df)
+xyplot(log(repo.stargazers_count,base=10)~repo.name,groups=lang,data=info.df)
+densityplot(~log(repo.stargazers_count,base=10),groups=lang,data=info.df)
+dotplot(lang~log(repo.stargazers_count,base=10),groups=lang,data=info.df)
+sp <- stripplot(
+  log(repo.stargazers_count,base=10)~lang,
+  groups=lang,
+  labels = info.df$repo.name,
+  data=info.df,
+  panel = function(x,y,labels,...){
+    panel.superpose(
+      x, y, ...,
+      panel.groups = function(x,y,subscripts,col.symbol,...){
+        panel.xyplot(x=x,y=y,subscripts=subscripts,col.symbol=col.symbol,...)
+        panel.text(
+          x=x[1],
+          y=y[1],
+          labels=labels[subscripts[1]],
+          col=col.symbol
+        )
+      }
+    )
+  }
+)
+
+require(directlabels)
+
+direct.label(
+  sp,top.bumpup)
+  dl.combine(top.bumpup,extreme.points,chull.grid,empty.grid)
+)
